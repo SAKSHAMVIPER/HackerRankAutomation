107cb82eaa02ee79fe5e286c833af3842ff7c233
SAKSHAMVIPER <sakshamsinghrkt387@gmail.com>
2021-09-27 13:52:18 +0530

    next commit

--- challenges.js
+++ challenges.js
@@ -2,6 +2,7 @@ const puppy  = require("puppeteer");
 
 
 let moderatrors = ["maiya_o", "SAKSHAMVIPER","KingGeneral","sp24174871","praveenthedev ","mithumhetre0001","qkrrbtjd90","davidmcgowan001","fsfsdf5","yashpalsinghdeo1"];
+let challenges = ["practice0","practice1","practice2","practice3","practice4","practice5","practice6","practice7","practice8","practice9"];
 async function openBrowser(url){
     const browser = await puppy.launch({
         headless: false,   //for visibility of browser
@@ -57,13 +58,24 @@ let loginclick = await tab.$('[data-analytics="LoginPassword"]');
                                 let administrationtabs = await tab.$$(".admin-tabbed-nav a");
                                 await administrationtabs[1].click();
                                 await tab.waitForSelector(".btn.btn-green.backbone.pull-right");
-                                let createchallenges =  await tab.$(".btn.btn-green.backbone.pull-right");
-                                await createchallenges.click();
+                                let createChallengeButton =  await tab.$(".btn.btn-green.backbone.pull-right");
+                                let createChallengeUrl = await tab.evaluate(function(ele){
+                                  return ele.getAttribute("href");
+                              },createChallengeButton);
+                              for(let i = 0; i < challenges.length; i++) {
+                                  await createChallenge("https://www.hackerrank.com" + createChallengeUrl,challenges[i],tab)
+                              }
+                          }
+
+
+                           async function createChallenge(url,challenge,tab){
+                               // await createchallenges.click();
+                               await tab.goto(url);
                                 await tab.waitForSelector("#input_format-container .CodeMirror-code");
                                 let challengesname = await tab.$("#name");
                                 let challengespreview = await tab.$("#preview");
-                                await challengesname.type("newhello");
-                                await challengespreview.type("just for fun");
+                                await challengesname.type(challenge);
+                                await challengespreview.type(challenge);
 
                                 let codeTextAreas = await tab.$$(".CodeMirror-code");
 
@@ -72,13 +84,13 @@ let loginclick = await tab.$('[data-analytics="LoginPassword"]');
                                 });
                                 for(let i in codeTextAreas) {
                                     await codeTextAreas[i].click();
-                                    await codeTextAreas[i].type("kjdsbf");
+                                    await codeTextAreas[i].type(challenge);
                             }
 
                             await tab.waitForSelector("#tags_tagsinput");
                             let taginput = await tab.$("#tags_tagsinput");
                             await taginput.click();
-                            await taginput.type("jhjofhwr");
+                            await taginput.type(challenge);
                             await tab.keyboard.press("Enter");
                             await tab.waitForSelector(".save-challenge.btn.btn-green")
                             let saveChangesButton = await tab.$(".save-challenge.btn.btn-green");
@@ -95,10 +107,7 @@ let loginclick = await tab.$('[data-analytics="LoginPassword"]');
                           await tab.waitForSelector(".save-challenge.btn.btn-green")
                           let savechangeagain = await tab.$(".save-challenge.btn.btn-green")
                           await savechangeagain.click();
-                        //  await tab.waitForSelector('[data-analytics="Breadcrumb"]');
-                        //   let moveback = await tab.$$('[data-analytics="Breadcrumb"]');
-                        //   await moveback[0].click();
-                        await page.goBack();
+                        
 
 
 
